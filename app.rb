53c8f1e0d336f738a6cb598da904bd5e14a945ae
Emily Bernstein <ebernst6@binghamton.edu>
2018-09-15 22:53:57 +0000

    Yay, it actually passes the test

--- app.rb
+++ app.rb
@@ -62,17 +62,24 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    
+    if @game.check_win_or_lose == :win
+        redirect '/win'
+    elsif @game.check_win_or_lose == :lose
+        redirect '/lose'
+    end
     erb :show # You may change/remove this line
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
+    flash[:message] = "You Win!"
     erb :win # You may change/remove this line
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
+    flash[:message] = "Sorry, you lose!"
+  
     erb :lose # You may change/remove this line
   end
   
