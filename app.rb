7c6860f6af8848766cf57c083fce725557099312
Emily Bernstein <ebernst6@binghamton.edu>
2018-09-15 22:29:36 +0000

    now flashes on inccorrect/invalid

--- app.rb
+++ app.rb
@@ -41,7 +41,16 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    @game.guess letter
+    
+    begin
+        status = @game.guess letter
+    rescue ArgumentError => e
+      flash[:message] = "You have already used that letter."
+    end
+    if not status
+        flash[:message] = "Invalid guess"
+    end
+    
     redirect '/show'
   end
   
